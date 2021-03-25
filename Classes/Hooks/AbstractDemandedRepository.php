4f430bfc6cfe8ddbeaa149427fb2f3c25db65956
Christian Welzel <c.welzel@schech.net>
2021-03-25 12:35:57 +0100

    Add a "filter by location" field to the backend form.

--- Classes/Hooks/AbstractDemandedRepository.php
+++ Classes/Hooks/AbstractDemandedRepository.php
@@ -51,6 +51,7 @@ class AbstractDemandedRepository
         array &$constraints
     ) {
         $eventRestriction = $demand->getEventRestriction();
+        $eventFilterByLocation = $demand->getEventFilterByLocation();
 
         /** @var QueryInterface $query */
         if ($eventRestriction === Demand::EVENT_RESTRICTION_NO_EVENTS) {
@@ -91,6 +92,10 @@ class AbstractDemandedRepository
             $locations = $demand->getLocations();
             if (!empty($locations)) {
                 $constraints[] = $query->in('location', $locations);
+            }
+
+            if (!empty($eventFilterByLocation)) {
+                $constraints[] = $query->equals('location', $eventFilterByLocation);
             }
 
             // Time start
